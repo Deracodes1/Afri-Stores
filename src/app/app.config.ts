c123a80fb6142f668669ac2151999de08fa038a3
Deracodes1 <ezemeniachidera@gmail.com>
2026-02-16 03:20:37 -0800

    added preloading to lazy loaded routes for better perfromance and UX

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { authInterceptor } from './interceptos/auth-interceptor';
-import { provideRouter } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withInterceptors([authInterceptor])), provideRouter(routes)],
+  providers: [
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+  ],
 };
